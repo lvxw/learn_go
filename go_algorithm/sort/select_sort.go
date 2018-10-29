b885986341e9895d62ade50f894fce1e2c495ec6
lvxw <2622478542@qq.com>
2018-10-29 22:40:30 +0800

    no message

--- go_algorithm/sort/select_sort.go
+++ go_algorithm/sort/select_sort.go
@@ -4,16 +4,18 @@ import "fmt"
 
 func main() {
 	var arr = []int{1,23,5,7,87,1,3,8,45,2,56,34,67,44}
-	bubble_sort(arr)
+	select_sort(arr)
 	fmt.Println(arr)
 }
 
-func bubble_sort(arr []int){
+func select_sort(arr []int){
 	for i:=0;i< len(arr)-1;i++{
+		init := i
 		for j:=i+1;j<len(arr);j++ {
-			if arr[i]>arr[j]{
-				arr[i],arr[j] = arr[j],arr[i]
+			if arr[j] < arr[i]{
+				init = j
 			}
 		}
+		arr[i],arr[init] = arr[init],arr[i]
 	}
 }
--- go_core/for/for_1.go
+++ go_core/for/for_1.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print(123)
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
 }
