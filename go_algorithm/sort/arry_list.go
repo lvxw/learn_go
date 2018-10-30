9b3953863d01a8ce16d38e17f2a1fa4346a77047
lvxw <lvxw@fun.tv>
2018-10-30 19:10:54 +0800

    no message

--- go_algorithm/sort/arry_list.go
+++ go_algorithm/sort/arry_list.go
@@ -7,20 +7,18 @@ type ArrayList struct {
 	size int
 }
 
-func add(list *ArrayList,el ...int){
+func (a *ArrayList) add(el ...int){
 	for i:=0;i<len(el);i++{
-		list.arr = append(list.arr, el[i])
-		list.size += 1
+		a.arr = append(a.arr, el[i])
+		a.size += 1
 	}
 }
 
 func main() {
 	list := new(ArrayList)
 
-	add(list,1,2,3,4,5,7,8,9,10)
+	list.add(1,2,3,4,5,7,8,9,10)
 
 	fmt.Println(list.arr)
 	fmt.Println(list.size)
-
-
 }
